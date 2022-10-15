725a16efb2bd832afd35777c64a55a661354eae0
Diego Baumbach <dbsmendes@gmail.com>
2022-10-15 05:57:58 -0300

    TRABALHO MODULO 2
    projeto individual modulo 2, desenvolver uma página simples que criptografe e decriptografe códigos em cifra de Cesar e base64.
    
    *Projeto em andamento*

---
+++ script/script.js
@@ -0,0 +1,123 @@
+var mensagemEntrada = document.getElementById("texto_entrada");
+var mensagemSaida = document.getElementById("texto_saida");
+var incremento = document.getElementById("num");
+
+function nomeBotao() { // modifica o nome do botao ao clicar no radio
+    let encripta = document.getElementById("encript");
+
+    if (encripta.checked == true) {
+        document.getElementById("btn").innerHTML = "Encriptar"
+    }
+    else {
+        document.getElementById("btn").innerHTML = "Decriptar"
+    }
+}
+function componenteUnhidden() { // FAZ APARECER O INPUT PARA PREENCHER COM O INCREMENTO DA CIFRA DE CESAR
+    let cifra = document.getElementById("cifra");
+
+    if (cifra.checked == true) {
+        document.querySelector("div.component").style.visibility = "visible";
+        document.querySelector("div.component").style.width = "280px";
+    }
+    else {
+        document.querySelector("div.component").style.visibility = "hidden";
+        document.querySelector("div.component").style.width = "1px";
+    }
+}
+
+
+    
+    
+function botao() {
+    if (mensagemEntrada.value.trim() == "") { // TESTA SE O CAMPO MENSAGEM ESTÁ VAZIO
+        document.getElementById("error").innerHTML = "Preencha o campo (Mensagem)";
+    }
+    else {
+            // CIFRA DE CESAR
+        if (document.getElementById("cifra").checked) {
+            cifra(mensagemEntrada, incremento, mensagemSaida); // CHAMA A FUNÇÃO PARA CRIPTOGRAFAR
+            mensagemSaida.style.width = "250px" // codigo para abrir a caixa saida
+            mensagemSaida.style.height = "100px"
+            mensagemSaida.style.visibility = "visible"
+        }
+            // BASE64
+        else {
+            base(mensagemEntrada, mensagemSaida);
+            mensagemSaida.style.width = "250px" // codigo para abrir a caixa saida
+            mensagemSaida.style.height = "100px"
+            mensagemSaida.style.visibility = "visible"
+        }   
+    }
+}
+
+// (CIFRA DE CESAR) - CRIPTOGRAFAR e DESCRIPTOGRAFAR
+
+function cifra(mensagemEntrada, incremento, mensagemSaida) {
+    mensagemEntrada = mensagemEntrada.value;
+    incremento = incremento.value;
+    var alfabeto = "abcdefghijklmnopqrstuvwxyz".split("");
+    var acento = "áàãâäéèêíìîóòôõúùûüç".split("");
+    var mensagemPronta = "";  // variável onde será montado a nova frase (codificada ou decodificada)
+    var fraseLowerCase = mensagemEntrada.toLowerCase();
+    incremento = incremento % 26;
+    for (var index = 0; index < fraseLowerCase.length; index++) {
+        var currentLetter = fraseLowerCase[index];
+        if (currentLetter === " ") {
+            mensagemPronta += currentLetter;
+            continue;
+        }
+        if (acento.indexOf(currentLetter) != -1) {
+            var currentLetter = currentLetter.normalize('NFD').replace(/[\u0300-\u036f]/g, ""); // substitui letras com acentos por letras sem acentos
+        }
+        if (alfabeto.indexOf(currentLetter) == -1) { // caso o usuário coloque caracteres especiais ou números, os mesmos serão adicionados a variável "mensagemPronta", sem precisar passar pela tratativa da cifra de cesar.
+            mensagemPronta += currentLetter; 
+        }
+        else{
+            var currentIndex = alfabeto.indexOf(currentLetter);
+            if (document.getElementById("encript").checked) { // VERIFICA SE É PARA ENCRIPTAR OU DECRIPTAR O CÓDIGO
+                var newIndex = parseInt(currentIndex) + parseInt(incremento); // ENCRIPTAR
+            }
+            else {
+                var newIndex = parseInt(currentIndex) - parseInt(incremento); // DECRIPTAR
+            }
+            if (newIndex > 25) newIndex = parseInt(newIndex) - 26;
+            if (newIndex < 0) newIndex = parseInt(newIndex) + 26;        
+            if (mensagemEntrada[index] === mensagemEntrada[index].toUpperCase()){
+                mensagemPronta += alfabeto[newIndex].toUpperCase();
+            }
+            else {
+                mensagemPronta += alfabeto[newIndex];
+            }
+        }
+        mensagemSaida.innerHTML = mensagemPronta;
+    }
+}
+
+// (BASE64) CRIPTOGRAFAR e DESCRIPTOGRAFAR
+
+function base(mensagemEntrada, mensagemSaida) {
+    var mensagemEntrada = mensagemEntrada.value;
+    var mensagemPronta = "";
+    if (document.getElementById("encript").checked) { // VERIFICA SE É PARA ENCRIPTAR OU DECRIPTAR O CÓDIGO
+        mensagemPronta = window.btoa(mensagemEntrada);
+        mensagemSaida.innerHTML = mensagemPronta;
+    }
+    else {
+        mensagemPronta = window.atob(mensagemEntrada);
+        mensagemSaida.innerHTML = mensagemPronta;
+    }
+}
+
+
+
+// TIRA MENSAGEM DE ERRO AO CLICAR NO TextArea "MENSAGEM"  + LIMPAR E FECHAR CAIXA DE TEXTO "SAIDA DO CODIGO PRONTO"
+function reseta() {
+    document.querySelector("p#error").innerHTML = "";
+
+    // codigo para fechar a caixa de saída da mensagem
+    mensagemSaida.style.width = "1px"; 
+    mensagemSaida.style.height = "1px";
+    mensagemSaida.style.visibility = "hidden";
+    mensagemSaida.innerHTML = "";
+
+}
